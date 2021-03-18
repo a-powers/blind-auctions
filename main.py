a6bbdcfc34aca772bd64cff1f2e75cee3d2381be
apowers2 <awpowers@gmail.com>
2021-03-18 14:04:47 +0000

    update

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
-all_bids = {}
-active_bids = False
-
-while not active_bids:
-    name = input("What is your name? ")
-    bid = int(input("What is your bid? $"))
-
-    all_bids[name] = bid
-    more_bidders = input("Are there more bidders? ")
-    if more_bidders == "n":
-        active_bids = True
-        
-    
-highest_bid = 0
-for i in all_bids:
-    if all_bids[name] > highest_bid:
-        highest_bid = all_bids[name]
-print(f'The winner is {name} with the bid of {highest_bid}')
