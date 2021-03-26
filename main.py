7ce8183b4fa6cb7ace3d7a1f61a9430a888061ab
a-powers <awpowers@gmail.com>
2021-03-26 18:23:46 +0000

    update

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+print("Welcome to the Auction.")
 
+
+
+
+auction_bids = {}
+name1 = input("What is your name? ")
+bid1 = int(input("How much are you bidding? $"))
+auction_bids[name1] = bid1
+print(auction_bids)
