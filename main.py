b9c4953eef624b981ff3012710d184823612e4c5
a-powers <awpowers@gmail.com>
2021-03-29 14:32:20 +0000

    update

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
-print("Welcome to the Auction.")
-
-bidder = False
-
-while not bidder:
-
-    auction_bids = {}
-    name1 = input("What is your name? ")
-    bid1 = int(input("How much are you bidding? $"))
-    auction_bids[name1] = bid1
-    bidder = True
-    print(auction_bids)
