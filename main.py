e77aa619e5d61e8af0ff0acdc7b72419ee39466a
a-powers <awpowers@gmail.com>
2021-03-30 18:12:31 +0000

    update

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+
+bid_dict = {}
+continue_bidding = False
+
+
+bidder = input("What is your name? ")
+bid = int(input(What is your bid? ))
