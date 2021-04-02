dc55f7c4cea512015b31b6311d6a1b09b64cc04f
a-powers <>
2021-04-02 00:51:54 +0000

    update

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
 
-bid_dict = {}
+bid_list = []
 continue_bidding = True
 
 
 while continue_bidding:
+    bid_dict = {}
     bidder = input("What is your name? ")
     bid = int(input("What is your bid? "))
     bid_dict[bidder] = bid
