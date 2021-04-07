02ec5d331b5b5267cbe87b5ba1dc5d8818dfa17a
a-powers <>
2021-04-07 15:21:49 +0000

    update

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from replit import clear
 #HINT: You can call clear() to clear the output in the console.
+
+bid_dict = {}
+continue_bidding = False
+
+while not continue_bidding:
+    bidder = input("What is your name? ")
+    bid = int(input("What is your bid? "))
+    more_bidders = input("Are there more bidders? ")
+    if more_bidders == 'no':
+        continue_bidding = True
+    else:
+        bid_dict[bidder] = bid
+    print(bid_dict)
+    
